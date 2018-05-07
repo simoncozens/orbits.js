873ae37795361f9860665839dbcf7d6af2ad124c
Simon Cozens <simon@simon-cozens.org>
2018-05-07 21:23:21 +1000

    Oops

---
+++ src/start.js
@@ -0,0 +1,10 @@
+Engine = require("./engine").Engine;
+$(function() {
+  Engine.init($("#canvas"));
+  console.log(Engine);
+  Engine.drawSelf();
+  $("#original").click(function() { Engine.load("original.sav"); })
+  $("#sol6abort").click(function() { Engine.load("sol6abort.sav"); })
+  $("#final").click(function() { Engine.load("final.sav"); })
+});
+
